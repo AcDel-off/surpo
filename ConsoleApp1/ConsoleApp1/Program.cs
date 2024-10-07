1c86c4726631d12830a9200431e5749d01d1103c
AcDel-off <145152125+AcDel-off@users.noreply.github.com>
2024-10-07 13:52:00 +0300

    added c# project

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isHidden = false;
+            Console.WriteLine($"Hidden status: {isHidden}");
+        }
+    }
+}
